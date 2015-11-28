14450c83172e3ea8580726e1bc950f4495fb9879
Olaf Klischat <o.klischat@syseleven.de>
2015-11-28 19:06:18 +0100

    test runner runs test contained in subdirectories

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-../base-passwd/update-passwd -P passwd -p passwd.master -G group -g group.master -S shadow -L "$@"
+program=../base-passwd/update-passwd
+
+for t in test-*; do
+    mkdir -p testrun
+    rm -rf testrun/*
+    cp -f "$t"/* testrun/
+    "$program" -P testrun/passwd -p testrun/passwd.master -G testrun/group -g testrun/group.master -S testrun/shadow -L "$@"
+    failed=0
+    diff -u testrun/passwd.expected testrun/passwd || failed=1
+    diff -u testrun/group.expected testrun/group || failed=1
+    diff -u testrun/shadow.expected testrun/shadow || failed=1
+    [ "$failed" != 0 ] && exit 1
+done
+
+echo SUCCESS.
 
